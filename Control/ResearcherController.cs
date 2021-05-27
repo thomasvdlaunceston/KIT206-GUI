53227b98e9683da3c30953a4baeefa3742de7097
clearspec11 <57431215+clearspec11@users.noreply.github.com>
2021-05-27 13:46:43 +1000

    now search filters by last name

--- Control/ResearcherController.cs
+++ Control/ResearcherController.cs
@@ -49,8 +49,10 @@ namespace KIT206_GroupWork.Control
         {
             //Only Filters BY FIRST NAME
             var filtered = from Researcher.Researcher res in mainList
-                           where res.GivenName.ToLower().Contains(name.ToLower())
+                           where res.FamilyName.ToLower().Contains(name.ToLower()) || res.GivenName.ToLower().Contains(name.ToLower())
+                           
                            select res;
+
             displayList.Clear();
             filtered.ToList().ForEach(displayList.Add);
         }
